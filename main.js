921ff883e97d87ed570b5f0ec7bc8ae78826f013
Beny <b.ntambwe97@gmail.com>
2021-01-26 09:57:41 +0100

    fin exo

---
+++ main.js
@@ -0,0 +1,17 @@
+// 1
+
+let montitre = document.getElementsByTagName('h1') 
+console.log(montitre);
+
+// 2
+
+let soustitre = document.getElementsByTagName('h3')
+console.log(soustitre[0,1,2]);
+
+// 3
+
+let paragraph = document.getElementsByTagName('p')
+console.log(paragraph[1]);
+// 4
+let li = document.getElementsByTagName('li')
+console.log(li[2]);
